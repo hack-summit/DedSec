ecefe72b857187cafbb68247b44ee54993e61113
Sreenivasa AR <50909209+SreeAR@users.noreply.github.com>
2019-08-31 19:30:30 +0530

    A small update.
    About the request methods, there was a small correction had to be done and is completed now.

--- click_jacking.py
+++ click_jacking.py
@@ -10,39 +10,35 @@ class click_jacking():
         global print1
         global bool_flag
         r = requests.get(username)
-        bool_flag = True
-        print1 = "this isn't get request, so it is not vulnerable to clickjacking."
+        bool_flag = False
         a = r.headers
-        if (request.method == "GET"):
-            print1 = "this domain doesn't allow html as content-type.so, it is not vulnerable to click-jacking."
-            if ("text/html" in a["Content-Type"]):
+
+        print1 = "this domain doesn't contains x-frame options."
+        if ("X-Frame-Options" in r.headers):
+            if (("DENY" in a["X-Frame-Options"]) or ("SAMEORIGIN" in a["X-Frame-Options"])):
+                bool_flag = True
+                print1 = "it is not vulnerable to clickjacking as it contains x-frame options."
+            else:
+                print1 = "it is vulnerable click-jacking."
+        elif ("Content-Security-Policy" in r.headers):
+            bool_flag = True
+            print1 = "this domain doesn't contains frame ancestors in csp policy also it contains x-frame options, so it is not vulnerable to click-jacking."
+            if ("frame-ancestors" in a["Content-Security-Policy"]):
+                insecure_ancestors = ('*',
+                                      'http', 'https',
+                                      'http://', 'https://',
+                                      'http://*', 'https://*')
+                if (insecure_ancestors in a["Content-Security-Policy"]):
+                    bool_flag = False
+                    print1 = "this domain contains csp policy's insecure wilcards, it is vulnerable to click-jacking."
+                else:
+                    print1 = "it is not vunerable to click-jacking."
+            else:
                 bool_flag = False
-                print1 = "this domain doesn't contains x-frame options."
-                if ("X-Frame-Options" in r.headers):
-                    if (("DENY" in a["X-Frame-Options"]) or ("SAMEORIGIN" in a["X-Frame-Options"])):
-                        bool_flag = True
-                        print1 = "it is not vulnerable to clickjacking as it contains x-frame options."
-                    else:
-                        print1 = "it is vulnerable click-jacking."
-                elif ("Content-Security-Policy" in r.headers):
-                    bool_flag = True
-                    print1 = "this domain doesn't contains frame ancestors in csp policy also it contains x-frame options, so it is not vulnerable to click-jacking."
-                    if ("frame-ancestors" in a["Content-Security-Policy"]):
-                        insecure_ancestors = ('*',
-                                              'http', 'https',
-                                              'http://', 'https://',
-                                              'http://*', 'https://*')
-                        if (insecure_ancestors in a["Content-Security-Policy"]):
-                            bool_flag = False
-                            print1 = "this domain contains csp policy's insecure wilcards, it is vulnerable to click-jacking."
-                        else:
-                            print1 = "it is not vunerable to click-jacking."
-                    else:
-                        bool_flag = False
-                        print1 = "it is vulnerable to clickjacking"
+                print1 = "it is vulnerable to clickjacking"
         print(r.headers)
         print(request.headers)
-        js_data=request.form['']
+        # js_data=request.form['']
         print()
 
         return render_template("scan.html", username=username, print1=print1, bool_flag=bool_flag)
