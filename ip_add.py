e192a9df8dc9e32fa1d91508160bf5c547c14ced
Sreenivasa AR <50909209+SreeAR@users.noreply.github.com>
2019-08-31 21:44:02 +0530

    IP address of the url can be extracted from this.
    This file basically detects the IP address and the location of the server

---
+++ ip_add.py
@@ -0,0 +1,43 @@
+import socket
+
+import geoip2 as geoip2
+import requests
+from flask import request
+import geoip2.database
+
+class ip:
+
+    def geo_Ip(self):
+
+        global username
+        username = request.args.get('url')
+        req1 = requests.get(username)
+        a_1=req1.headers
+        server=a_1['Server']
+
+        list1 = username.split("//")
+
+        list2 = list1[1].split("/")
+
+        list3 = list2[0].split(".")
+
+        str1 = []
+        for i in range(len(list3)):
+            if "www" != list3[i]:
+                str1.append(list3[i])
+        s = "."
+
+        host_name=s.join(str1)
+        print(host_name)
+        a=socket.gethostbyname(host_name)
+
+        reader = geoip2.database.Reader('/Users/jaswa-zuch749/PycharmProjects/Learning/GeoLite2-City.mmdb')
+
+        response = reader.city(a)
+        country=response.country.iso_code
+        latitude=response.location.latitude
+        longitude=response.location.longitude
+
+
+
+        return a,country,latitude,longitude,server
