89db8d4ea748bb3ed653b6be8038b14606df634c
Sreenivasa AR <50909209+SreeAR@users.noreply.github.com>
2019-09-01 09:42:07 +0530

    Updated Server
    As we added XSS security checking in it we made some changes

--- Server.py
+++ Server.py
@@ -4,17 +4,18 @@ from requests import cookies
 
 from click_jacking import click_jacking
 from ip_add import ip
+from xss_protection import xss_Protection
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('hackathon.html')
+    return render_template('home.html')
 
 @app.route('/home_night')
 def night_1():
-    return render_template('hackathonnight_1.html')
+    return render_template('home_night.html')
 
 @app.route("/about")
 def about():
@@ -23,6 +24,22 @@ def about():
 @app.route('/about_night')
 def about_night():
     return render_template('about_night.html')
+
+@app.route('/severity')
+def severity():
+    return render_template('severity.html')
+
+@app.route('/severity_night')
+def severity_night():
+    return render_template('severity_night.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/contact_night')
+def contact_night():
+    return render_template('contact_night.html')
 
 
 @app.route('/scan')
@@ -34,7 +51,11 @@ def scan():
     geo=ip()
     c=geo.geo_Ip()
 
-    return render_template("scan.html",username=b[0],print1=b[1],bool_flag=b[2],ip=c[0],country=c[1],latitude=c[2],longitude=c[3],server=c[4])
+
+    xss=xss_Protection()
+    d=xss.xss_protect()
+
+    return render_template("scan.html",username=b[0],print1=b[1],bool_flag=b[2], ip = c[0], country = c[1], latitude = c[2], longitude = c[3], server = c[4],print2=d[0],bool_flag2=d[1])
 
 
 if __name__ == "__main__":
